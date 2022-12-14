c9086b33db165278aee051cd8126c294fa02ea43
Jacek <jaceman44@gmail.com>
2022-12-14 23:38:09 +0100

    Quick f4 setup

---
+++ Src/main.c
@@ -0,0 +1,16 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : J. Prokopczuk
+ * @brief          : Main program body
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+#include <stm32f4xx_hal_gpio.h>
+
+int main(void)
+{
+    /* Loop forever */
+	for(;;);
+}
