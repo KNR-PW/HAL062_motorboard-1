4f5fb22e3eb1ece042d03f0de56e6531a2fc4af2
Padamadam <arybojad@gmail.com>
2023-02-01 16:04:59 +0100

    Added comments and pulseNumber conversion for positive direction

--- Modules/motors/timers.c
+++ Modules/motors/timers.c
@@ -288,41 +288,51 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base) {
 
 }
 
+// @brief interrupt for angular speed calculation
+// @param htim handler for TIM7 interrupt clock
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	if (htim->Instance == TIM7) {
 
+		// getting actual position of encoder
 		enc1PulseNumber = __HAL_TIM_GET_COUNTER(&htim1);
 		enc2PulseNumber = __HAL_TIM_GET_COUNTER(&htim2);
 		enc3PulseNumber = __HAL_TIM_GET_COUNTER(&htim3);
 
 		// checking if direction is negative
+		// if position decreases from high value then direction is negative
+		// uint16_t converted to int16_t and set as negative value
 		if (enc1PulseNumber > ENC1_MAX_PULSE_VALUE / 2) {
 			enc1PulseNumber = ENC1_MAX_PULSE_VALUE - enc1PulseNumber;
 			enc1PulseNumber = (int16_t) enc1PulseNumber;
 			enc1PulseNumber = -1 * enc1PulseNumber;
+		} else
+			(int16_t) enc1PulseNumber;
 
-		}
 		if (enc2PulseNumber > ENC2_MAX_PULSE_VALUE / 2) {
 			enc2PulseNumber = ENC2_MAX_PULSE_VALUE - enc2PulseNumber;
 			enc2PulseNumber = (int16_t) enc2PulseNumber;
 			enc2PulseNumber = -1 * enc2PulseNumber;
+		} else
+			(int16_t) enc2PulseNumber;
 
-		}
 		if (enc3PulseNumber > ENC3_MAX_PULSE_VALUE / 2) {
 			enc3PulseNumber = ENC3_MAX_PULSE_VALUE - enc3PulseNumber;
 			enc3PulseNumber = (int16_t) enc3PulseNumber;
 			enc3PulseNumber = -1 * enc3PulseNumber;
+		} else
+			(int16_t) enc3PulseNumber;
 
-		}
-
+		// evaluating number of pulses occured in one sec
 		enc1PulsePerSec = enc1PulseNumber * 1000 / VELOCITY_CLOCK_TIME;
 		enc2PulsePerSec = enc2PulseNumber * 1000 / VELOCITY_CLOCK_TIME;
 		enc3PulsePerSec = enc3PulseNumber * 1000 / VELOCITY_CLOCK_TIME;
 
+		// evaluating angular speed basing on encoder resolution
 		motor1Velocity = enc1PulsePerSec / ENC1_PULSE_PER_ROTATION;
 		motor2Velocity = enc2PulsePerSec / ENC2_PULSE_PER_ROTATION;
 		motor3Velocity = enc3PulsePerSec / ENC3_PULSE_PER_ROTATION;
 
+		// resetting counter for next interrupt
 		__HAL_TIM_SET_COUNTER(&htim1, 0);
 		__HAL_TIM_SET_COUNTER(&htim2, 0);
 		__HAL_TIM_SET_COUNTER(&htim3, 0);
