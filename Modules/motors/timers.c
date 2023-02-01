fb8b7f072ece4efaefc4293ac3cb3bcace775d36
czechos02 <01168879@pw.edu.pl>
2023-02-01 16:32:51 +0100

    adding htim5

--- Modules/motors/timers.c
+++ Modules/motors/timers.c
@@ -146,18 +146,17 @@ void TIM3_Init() {
 }
 
 void TIM5_Init() {
-	TIM_HandleTypeDef htim;
-	htim.Instance = TIM5;
-	htim.Init.Prescaler = PWM_PRESCALER;
-	htim.Init.CounterMode = TIM_COUNTERMODE_UP;
-	htim.Init.Period = PWM_MAX_DUTY;
-	htim.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-	htim.Init.RepetitionCounter = PWM_REPETITION_COUNTER;
-	htim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
-	if (HAL_TIM_PWM_Init(&htim) != HAL_OK) {
+	htim5.Instance = TIM5;
+	htim5.Init.Prescaler = PWM_PRESCALER;
+	htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim5.Init.Period = PWM_MAX_DUTY;
+	htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim5.Init.RepetitionCounter = PWM_REPETITION_COUNTER;
+	htim5.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+	if (HAL_TIM_PWM_Init(&htim5) != HAL_OK) {
 		// TODO: error handling needed
 	}
-	if (HAL_TIM_PWM_Start(&htim, TIM_CHANNEL_1 | TIM_CHANNEL_2 | TIM_CHANNEL_3)
+	if (HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_1 | TIM_CHANNEL_2 | TIM_CHANNEL_3)
 			!= HAL_OK) {
 		// TODO: error handling needed
 	}
